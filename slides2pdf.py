e3b6446414017dd65b4dfb76d2846295191ea97a
Seb (MBP) <seb@eskd.net>
2025-10-14 17:15:45 +0200

    Ready for class 1

---
+++ slides2pdf.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+
+
+def run_decktape(fl):
+    cmd = [
+        "decktape",
+        "reveal",
+        f"_site/slides/{fl}.html",
+        f"_site/slides/{fl}.pdf",
+    ]
+    try:
+        subprocess.run(cmd, check=True)
+        print("PDF generated successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    fl = sys.argv[1]
+    
+    cmd = [
+        "quarto",
+        "render",
+        f"slides/{fl}.qmd"]
+    
+    subprocess.run(cmd, check=True)
+    
+    run_decktape(fl)
+
