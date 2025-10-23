2a66433e53033cdbce4e730c1849028536da14ab
Lionel Voirol <lionelvoirol@hotmail.com>
2025-10-23 11:26:39 +0200

    finalized exercise for class 3

--- class3/test_wilcoxon.R
+++ class3/test_wilcoxon.R
@@ -26,3 +26,25 @@ dietC = diet$weight.loss[diet$diet.type=="C"]
 
 # Perfom test
 wilcox.test(dietA, dietC, alternative = "less", mu = -0.95)
+
+
+
+
+
+
+
+
+###############################
+library(lterdatasampler)
+
+index1 = and_vertebrates$year > 2018
+index2 = and_vertebrates$species == "Coastal giant salamander"
+index3 = and_vertebrates$section == "CC"
+index4 = and_vertebrates$section == "OG"
+
+X = and_vertebrates[index1 & index2 & index3,]
+Y = and_vertebrates[index1 & index2 & index4,]
+
+boxplot(X$weight_g, Y$weight_g)
+t.test(X$weight_g, Y$weight_g)
+wilcox.test(X$weight_g, Y$weight_g)
